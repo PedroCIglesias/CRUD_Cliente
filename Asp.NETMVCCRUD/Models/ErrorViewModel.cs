e40d674edd3c1f7b9c52a33cdd84f7cc7297ae86
Pedro Iglesias <pedrocb.iglesias@gmail.com>
2022-10-17 14:01:52 -0300

    Pasta desnecessaria

--- Asp.NETMVCCRUD/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Asp.NETMVCCRUD.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
